b1949105f3a935d04984c75abeea40703dd63842
Hyemie <hyemhamgoa1111@gmail.com>
2024-11-18 13:49:18 +0900

    bootstrap version link 추가

--- mail_system/src/App.js
+++ mail_system/src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
       <MailList />
-      <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+      <link
+        rel="stylesheet"
+        href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+      />
+      <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
       <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </div>
   );
